openwrt/firewall3

commit ffba75c9cd8f96ef34b7b3ab35315ce66ba8f37d

iptables: free xtables_match if found in need_protomatch
Fix Coverity Scan ID 1529284 by freeing the xtables_match from
xtables_find_match if found.

Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

iptables.c
@@ -709,12 +709,16 @@ init_match(struct fw3_ipt_rule *r, struct xtables_match *m, bool no_clone)
 static bool
 need_protomatch(struct fw3_ipt_rule *r, const char *pname)
 {
+	struct xtables_match *match;
+
 	if (!pname)
 		return false;
 
-	if (!xtables_find_match(pname, XTF_DONT_LOAD, NULL))
+	match = xtables_find_match(pname, XTF_DONT_LOAD, NULL);
+	if (!match)
 		return true;
 
+	free(match);
 	return !r->protocol_loaded;
 }
 

commit bf1d5fdf623473419f8f738fc9071fee84cb13e5

iptables: fix regression with unintended free in need_protomatch
xtables_find_match memory allocation is funny. It can return something
allocated in a linked list or return a just allocated match clone and is
never freed.

This caused confusion and made a broken patch where an unintended free
is done in the case of entry not cloned.
xtables_find_match have a way to comunicate that the entry is cloned by
returning a looping linked list where the next entry is the same entry.
We can use this to understand where the entry has to be freed.

Fixes: ffba75c9cd8f ("iptables: free xtables_match if found in need_protomatch")
Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>
Tested-by: Rui Salvaterra <rsalvaterra@gmail.com>

iptables.c
@@ -718,7 +718,9 @@ need_protomatch(struct fw3_ipt_rule *r, const char *pname)
 	if (!match)
 		return true;
 
-	free(match);
+	/* Free any kind of clone from xtables_find_match */
+	if (match == match->next)
+		free(match);
 	return !r->protocol_loaded;
 }
 